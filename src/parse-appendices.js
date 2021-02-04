80113f9ac79c0f48dc71b24c9c3170dbaef546eb
SeanCdepu <scarmichael@deputy.com>
2021-02-05 01:08:05 +1100

    Update parse-appendices cutting the redundent code

--- src/parse-appendices.js
+++ src/parse-appendices.js
@@ -29,30 +29,19 @@ const loadFile = (fileName) => {
     // console.dir(easyArray)
     try {
         easyArray.forEach((text, textsIndex) => {
-            // if (easyArray[textsIndex + 1] === consts.appendicesBreakPoint) {
-            //     finishappendices = true
-            //     console.log(`finishing at ${easyArray[textsIndex + 1]}`)
-            // }
+
             if (text === "C: CREATURES BY CHALLENGE") {
-                // console.log('equal:', "'" + text + "'", "'" + appendix + "'")
                 let i = textsIndex + 1
                 while (easyArray[i] !== 'CHALLENGE 0 (0 XP)') {
                     i += 1
                 }
                 i += 1
-                // console.log('i =', i)
-                // console.log(`easyArray[${i}] = ${easyArray[i]}`)
+
                 while (easyArray[i] !== 'D: CREATURES BY TERRAIN') {
                     if (finishappendices) {
                         throw BreakException
                     }
-                    // console.log('current: ' + easyArray[i])
-                    // console.log(
-                    //     'potential next page num char: ' +
-                    //         easyArray[i + 3].toString().charAt(0)
-                    // )
                     let incrementalObjectIndices = 3
-
 
                     if(easyArray[i].includes('CHALLENGE')) {
                         i += 1
@@ -60,7 +49,6 @@ const loadFile = (fileName) => {
 
                     if(easyArray[i].includes('@gmail.com')) {
                         if (easyArray[i+2] === 'D: CREATURES BY TERRAIN') {
-                            textsIndex = i
                             throw BreakException
                             break
                         }
@@ -74,51 +62,7 @@ const loadFile = (fileName) => {
 
                     if (appendixObject.creature === consts.appendicesBreakPoint)
                         finishappendices = true
-                    //let j = ''
-                    // if (easyArray[i + 3].toString().charAt(0) === '–') {
-                    //     // console.log('page of: ' + appendixObject.page)
-                    //     // console.log('dashed field: ' + easyArray[i + 3])
-                    //     if (easyArray[i + 3].toString() === '–') {
-                    //         j = easyArray[i + 3].toString()
-                    //         i += 1
-                    //     }
-                    //     if (/([0-9]|\-)+/g.test(easyArray[i + 3])) {
-                    //         // console.log('passed first')
-                    //         appendixObject.page = appendixObject.page.concat(
-                    //             j,
-                    //             easyArray[i + 3]
-                    //         )
-                    //         incrementalObjectIndices += 1
-                    //     } else if (
-                    //         /([a-zA-Z]|\-)+/g.test(easyArray[i + 3])
-                    //     ) {
-                    //         // console.log('passed second')
-                    //         appendixObject.creature = appendixObject.creature.concat(
-                    //             easyArray[i + 3]
-                    //         )
-                    //         incrementalObjectIndices += 1
-                    //     } else if (/^\–/g.test(easyArray[i + 3])) {
-                    //         // console.log('passed third')
-                    //         const previous = parseInt(appendixObject.page)
-                    //         const next = parseInt(easyArray[i + 4])
-                    //         if (
-                    //             Number.isInteger(previous) &&
-                    //             Number.isInteger(next)
-                    //         ) {
-                    //             appendixObject.page = appendixObject.page.concat(
-                    //                 easyArray[i + 3],
-                    //                 easyArray[i + 4]
-                    //             )
-                    //         } else {
-                    //             appendixObject.creature = appendixObject.creature.concat(
-                    //                 easyArray[i + 3],
-                    //                 easyArray[i + 4]
-                    //             )
-                    //         }
-                    //         incrementalObjectIndices += 2
-                    //     }
-                    //     // console.log('\n')
-                    // }
+
                     while(!Number.isInteger(parseInt(easyArray[i + 2]))){
                         appendixObject.creature = appendixObject.creature.concat(easyArray[i + 1])
                         i += 1
